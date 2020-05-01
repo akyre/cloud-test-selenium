b32dae5bf9c7f5514931c9b0e8715e414b57737f
Kilian Perrier <kilian.perrier@epitech.eu>
2020-05-01 14:37:37 +0200

    :label: Replacing 'title' by 'post'.

--- poc.js
+++ poc.js
@@ -13,11 +13,11 @@ function writeDataAs(name, data) {
     try {
         await driver.get("https://news.ycombinator.com");
         articles = await driver.findElement(By.className("itemlist"));
-        titles = await articles.findElements(By.className("storylink"));
+        posts = await articles.findElements(By.className("storylink"));
         authors = await articles.findElements(By.className("hnuser"));
     } finally {
-        titles = await Promise.all(titles.map(title => title.getText()));
-        writeDataAs("titles.json", JSON.stringify(titles, null, '\t'));
+        posts = await Promise.all(posts.map(post => post.getText()));
+        writeDataAs("posts.json", JSON.stringify(posts, null, '\t'));
         authors = await Promise.all(authors.map(author => author.getText()));
         writeDataAs("authors.json", JSON.stringify(authors, null, '\t'));
         await driver.quit();
